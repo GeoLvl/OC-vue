fd6efd9a5660e003a16af0d9231ec4e8c1c140ad
geoffrey <laville.geoffrey@gmail.com>
2022-06-06 11:58:31 +0200

    use

---
+++ src/components/Radio.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="radio">
+        <h2>Radio</h2>
+        <button @click="sendMessage">Envoyer un message</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'the-Radio',
+    methods: {
+        sendMessage() {
+            this.$emit('message-sent', {
+                message: 'mon magnifique message'
+            })
+        }
+    }
+}
+</script>
+
+<style scoped>
+.radio {
+    margin-bottom: 15px;
+}
+</style>
---
+++ src/components/Receiver.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Récepteur</h1>
+        <radio @message-sent="updateMessage"/>
+        {{ message }}
+    </div>
+</template>
+
+<script>
+import Radio from "./Radio.vue"
+
+export default {
+    name: 'the-Receiver',
+    components: {
+        Radio,
+    },
+    methods: {
+        updateMessage(payload) {
+            this.message = payload.message
+        }
+    },
+    data: () => ({
+        message: "Pas de messages"
+    })
+}
+</script>
+
+<style scoped lang='scss'>
+
+</style>
